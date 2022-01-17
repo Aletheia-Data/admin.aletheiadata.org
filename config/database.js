e0a5dc694b310a6af98e9d545b4123edad26fb30
EnzoVezzaro <enzo-vezzaro@live.it>
2022-01-17 10:36:59 +0100

    release v1.0.1

--- config/database.js
+++ config/database.js
@@ -14,6 +14,7 @@ module.exports = ({ env }) => ({
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
         ssl: env.bool('DATABASE_SSL', true),
+        debug: false
       },
     },
   },
